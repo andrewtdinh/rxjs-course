3459cc731cfa42992708c5ea273356e8e7b35379
Andrew Dinh <andrewdinh74@yahoo.com>
2022-01-17 15:10:48 -0800

    Create an http observable for practice

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -36,6 +36,27 @@ export class AboutComponent implements OnInit {
         console.log(evt)
       })
 
+      const http$ = Observable.create(observer => {
+        fetch('/api/courses')
+          .then(response => {
+            return response.json();
+          })
+          .then(body => {
+            observer.next(body);
+            observer.complete();
+          })
+          .catch(err => {
+            observer.error(err);
+
+          })
+      })
+
+      http$.subscribe(
+        courses => console.log({ courses }),
+        noop,
+        () => console.log('Completed')
+      )
+
 
     }
 
